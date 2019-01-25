83292fe667fe443c880590bfa04eea8a23c90405
Paul Kreymborg <paul.kreymborg@softwareag.com>
2019-01-25 09:07:24 -0800

    Added assignment operator

--- src/compiler/parser/node_parsers/compound_expression/parse_compound_expression.cpp
+++ src/compiler/parser/node_parsers/compound_expression/parse_compound_expression.cpp
@@ -8,19 +8,46 @@
 
 namespace parser::compound_expression
 {
+    void check_for_tok_type(
+        TokenType expected_type,
+        const std::vector<Token> & toks,
+        unsigned long start_idx)
+    {
+        if (toks[start_idx].get_type() != expected_type)
+        {
+            throw ParserException(
+                "Expected " + token_type_to_string(expected_type) +
+                ", got " + token_type_to_string(toks[start_idx].get_type())
+            );
+        }
+    }
+
+    ParseResult<syntax_tree::compound_expression::operator_suffixes::AssignmentOperatorSuffix> parse_assignment_suffix(
+        const std::vector<Token> & toks,
+        unsigned long start_idx)
+    {
+        check_for_tok_type(TokenType::EQUALS_CH, toks, start_idx);
+
+        // one for the equals ch
+        unsigned long consumed = 1;
+
+        auto next_simple_exp_result = parser::simple_expression::parse_simple_expression(toks, start_idx + consumed);
+
+        consumed += next_simple_exp_result.token_count();
+
+        return ParseResult(
+            syntax_tree::compound_expression::operator_suffixes::AssignmentOperatorSuffix(
+                next_simple_exp_result.parsed_val()
+            ),
+            consumed
+        );
+    }
+    
     ParseResult<syntax_tree::compound_expression::operator_suffixes::AdditionOperatorSuffix> parse_addition_suffix(
         const std::vector<Token> & toks,
         unsigned long start_idx)
     {
-        TokenType expect_plus_type = toks[start_idx].get_type();
-
-        if (expect_plus_type != TokenType::PLUS_CH)
-        {
-            throw ParserException(
-                "Expected " + token_type_to_string(TokenType::PLUS_CH) +
-                ", got " + token_type_to_string(expect_plus_type)
-            );
-        }
+        check_for_tok_type(TokenType::PLUS_CH, toks, start_idx);
 
         // one for the plus ch
         unsigned long consumed = 1;
@@ -61,6 +88,17 @@ namespace parser::compound_expression
 
                 op_suffix_containers.push_back(suffix_cont);
                 consumed += add_parse_result.token_count();
+            }
+            else if (next_tok_type == TokenType::EQUALS_CH)
+            {
+                auto assign_parse_result = parse_assignment_suffix(toks, start_idx + consumed);
+
+                auto suffix_container = syntax_tree::compound_expression::operator_suffixes::OperatorSuffixContainer(
+                    assign_parse_result.parsed_val()
+                );
+
+                op_suffix_containers.push_back(suffix_container);
+                consumed += assign_parse_result.token_count();
             }
             else
             {
