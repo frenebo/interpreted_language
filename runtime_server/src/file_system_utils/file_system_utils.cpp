ea906a132b062f48d590dbb62ced15c6be08a779
Paul Kreymborg <paul.kreymborg@softwareag.com>
2019-01-28 12:00:30 -0800

    Added boost filesystem

---
+++ runtime_server/src/file_system_utils/file_system_utils.cpp
@@ -0,0 +1,11 @@
+#include "file_system_utils.hpp"
+// #include <filesyste
+#include <boost/filesystem.hpp>
+
+namespace file_system_utils
+{
+    bool FileSystemUtils::required_dirs_exist()
+    {
+        return false;
+    }
+}
---
+++ runtime_server/src/file_system_utils/file_system_utils.hpp
@@ -0,0 +1,13 @@
+#ifndef FILE_SYSTEM_UTILS_H
+#define FILE_SYSTEM_UTILS_H
+
+
+namespace file_system_utils
+{
+    class FileSystemUtils
+    {
+        static bool required_dirs_exist();
+    };
+}
+
+#endif
